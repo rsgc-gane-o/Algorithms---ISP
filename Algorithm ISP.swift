2618a41a559b46497c8105302070a491498280f0
Oliver Gane <ogane@rsgc.on.ca>
2016-01-17 12:57:55 -0500

    Fixed several errors, specifically "Expression pattern of type 'String' cannot match values of type 'String?”. All working now.

--- Algorithm ISP.swift
+++ Algorithm ISP.swift
@@ -1,6 +1,7 @@
+
 //
 //  Algorithm ISP.swift
-//  
+//
 //
 //  Created by Student on 2016-01-14.
 //
@@ -14,64 +15,64 @@ print("Welcome to Oliver's Unit Converter!")
 var validInput : Bool
 
 repeat {
-
+    
     print("\nWhat type of units are you working with today?")
     print("A - Mass")
     print("B - Capacitance")
     print("C - Temperature")
     print("D - Volume")
     print("Select what type of unit you want (A/B/C/D/Quit): ", terminator: "")
-
+    
     var chosenUnit = readLine(stripNewline: true)
     validInput = true
-
+    
     if let actualUnit = chosenUnit{
-
-    print("")
-
-    switch chosenUnit {
-
-    case "A", "a":
-        print("\nAwesome, what unit are you starting with?")
-        print("A - Gram")
-        print("B - Ounce")
-        print("C - Pound")
-        print("D - Kilogram")
-        print("Select what type of unit you are starting with (A/B/C/D/Quit: ", terminator: "")
-
-        var massStartingUnit = readLine(stripNewline: true)
-        validInput = true
-
-        if let acualMassStartingUnit = massStartingUnit{
+        
+        print("")
+        
+        switch actualUnit {
+            
+        case "A", "a" :
+            print("\nAwesome, what unit are you starting with?")
+            print("A - Gram")
+            print("B - Ounce")
+            print("C - Pound")
+            print("D - Kilogram")
+            print("Select what type of unit you are starting with (A/B/C/D/Quit: ", terminator: "")
+        default:
+            print("Sorry, please re-input letter")
+            validInput = false
+            
+            
+            var massStartingUnit = readLine(stripNewline: true)
+            validInput = true
+            
+            if let actualMassStartingUnit = massStartingUnit{
+                
+                print("")
+                
+                switch actualMassStartingUnit {
+                    
+                case "A" ,"a","B" ,"C" ,"D":
+                    print("\nOk, what are you converting to?")
+                    
+                    print("A - Gram")
+                    print("B - Ounce")
+                    print("C - Pound")
+                    print("D - Kilogram")
+                    print("Select what type of unit you are converting to (A/B/C/D/Quit: ", terminator: "")
+                default:
+                    print("Sorry, please re-input letter.")
+                    validInput = false
+                    
+                    
+                }
+            }
+        }
+    }
+} while validInput == false
 
 print("")
 
-switch massStartingUnit {
-
-case "A", "a":
-print("\nOk, what are you converting to?")
-print("A - Gram")
-print("B - Ounce")
-print("C - print("Select what type of unit you are starting with (A/B/C/D/Quit: ", terminator: "")
-print("D - Kilogram")
-print("Select what type of unit you are converting to (A/B/C/D/Quit: ", terminator: "")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-}
-}
-}
-}
