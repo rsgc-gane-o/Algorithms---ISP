924aa47a7b82a351ba63c81cc18ceeacf7daf280
Oliver Gane <ogane@rsgc.on.ca>
2016-01-17 21:45:50 -0500

    Fully finished code without documentation, still need to update IPO.txt this was created in the command line example file so will need to get this changed somehow to work with proper output. The code works but no output goes on screen in this specific file.

--- Algorithm ISP.swift
+++ Algorithm ISP.swift
@@ -8,7 +8,7 @@
 //
 
 import Foundation
-1
+
 
 
 print("Welcome to Oliver's Unit Converter!")
@@ -163,20 +163,20 @@ if unitType == 1 {
 
 if unitType == 2{
     print("\nWhat unit are you starting with?")
-    print("1 - Gram")
-    print("2 - Ounce")
-    print("3 - Pound")
-    print("4 - Kilogram")
+    print("1 - Picofarad")
+    print("2 - Nanofarad")
+    print("3 - Millifarad")
+    print("4 - Microfarad")
     print("Select what type of unit you are starting with (1/2/3/4/Quit)")
     
     var startingUnit = Int(readLine(stripNewline: true)!)!
     
     if startingUnit == 1 {
         print("What would you like to convert to?")
-        print("1 - Gram")
-        print("2 - Ounce")
-        print("3 - Pound")
-        print("4 - Kilogram")
+        print("1 - Picofarad")
+        print("2 - Nanofarad")
+        print("3 - Microfarad")
+        print("4 - Millifarad")
         var endingUnit = Int(readLine(stripNewline: true)!)!
         
         print("\nOk, please enter in the value you want to change.")
@@ -188,22 +188,303 @@ if unitType == 2{
             print(Value)
         }
         if startingUnit == 1 && endingUnit == 2{
-            Value = Value * 28.349523
+            Value = Value * 0.001
             print("And your new value is......")
             print(Value)
         }
         if startingUnit == 1 && endingUnit == 3{
-            Value = Value * 453.59237
+            Value = Value * 0.000001
             print("And your new value is......")
             print(Value)
         }
         if startingUnit == 1 && endingUnit == 4{
+            Value = Value * 0.000000001
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+    if startingUnit == 2 {
+        print("What would you like to convert to?")
+        print("1 - Picofarad")
+        print("2 - Nanofarad")
+        print("3 - Microfarad")
+        print("4 - Millifarad")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 2 && endingUnit == 1{
             Value = Value * 1000
             print("And your new value is......")
             print(Value)
         }
+        if startingUnit == 2 && endingUnit == 2{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 2 && endingUnit == 3{
+            Value = Value * 0.001
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 2 && endingUnit == 4{
+            Value = Value * 0.000001
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+    if startingUnit == 3 {
+        print("What would you like to convert to?")
+        print("1 - Picofarad")
+        print("2 - Nanofarad")
+        print("3 - Microfarad")
+        print("4 - Millifarad")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 3 && endingUnit == 1{
+            Value = Value * 1000000
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 3 && endingUnit == 2{
+            Value = Value * 1000
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 3 && endingUnit == 3{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 3 && endingUnit == 4{
+            Value = Value * 0.001
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+    if startingUnit == 4 {
+        print("What would you like to convert to?")
+        print("1 - Picofarad")
+        print("2 - Nanofarad")
+        print("3 - Microfarad")
+        print("4 - Millifarad")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 4 && endingUnit == 1{
+            Value = Value * 1000000000
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 4 && endingUnit == 2{
+            Value = Value * 1000000
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 4 && endingUnit == 3{
+            Value = Value * 1000
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 4 && endingUnit == 4{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+    }
 }
-
-
-
-
+if unitType == 3{
+    print("\nWhat unit are you starting with?")
+    print("1 - Celsius")
+    print("2 - Fahrenheit")
+    print("Select what type of unit you are starting with (1/2/Quit)")
+    
+    var startingUnit = Int(readLine(stripNewline: true)!)!
+    
+    if startingUnit == 1 {
+        print("What would you like to convert to?")
+        print("1 - Celsius")
+        print("2 - Fahrenheit")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 1 && endingUnit == 1{
+            Value = Value*1
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 1 && endingUnit == 2{
+            Value = Value * 9/5 + 32
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+    if startingUnit == 2 {
+        print("What would you like to convert to?")
+        print("1 - Celsius")
+        print("2 - Fahrenheit")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 2 && endingUnit == 1{
+            Value = (Value - 32) * 5/9
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 2 && endingUnit == 2{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+}
+if unitType == 4 {
+    print("\nWhat unit are you starting with?")
+    print("1 - Milliliter")
+    print("2 - Pint")
+    print("3 - Liter")
+    print("4 - Gallon")
+    print("Select what type of unit you are starting with (1/2/3/4/Quit)")
+    
+    var startingUnit = Int(readLine(stripNewline: true)!)!
+    
+    if startingUnit == 1 {
+        print("What would you like to convert to?")
+        print("1 - Milliliter")
+        print("2 - Pint")
+        print("3 - Liter")
+        print("4 - Gallon")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 1 && endingUnit == 1{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 1 && endingUnit == 2{
+            Value = Value * 0.002113
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 1 && endingUnit == 3{
+            Value = Value * 0.001
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 1 && endingUnit == 4{
+            Value = Value * 0.000264
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+    if startingUnit == 2 {
+        print("What would you like to convert to?")
+        print("1 - Milliliter")
+        print("2 - Pint")
+        print("3 - Liter")
+        print("4 - Gallon")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 2 && endingUnit == 1{
+            Value = Value * 473.176473
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 2 && endingUnit == 2{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 2 && endingUnit == 3{
+            Value = Value * 0.473176
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 2 && endingUnit == 4{
+            Value = Value * 0.125
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+    if startingUnit == 3 {
+        print("What would you like to convert to?")
+        print("1 - Milliliter")
+        print("2 - Pint")
+        print("3 - Liter")
+        print("4 - Gallon")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 3 && endingUnit == 1{
+            Value = Value * 1000
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 3 && endingUnit == 2{
+            Value = Value * 2.113376
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 3 && endingUnit == 3{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 3 && endingUnit == 4{
+            Value = Value * 0.264172
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+    if startingUnit == 4 {
+        print("What would you like to convert to?")
+        print("1 - Milliliter")
+        print("2 - Pint")
+        print("3 - Liter")
+        print("4 - Gallon")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 4 && endingUnit == 1{
+            Value = Value * 3785.411784
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 4 && endingUnit == 2{
+            Value = Value * 8
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 4 && endingUnit == 3{
+            Value = Value * 3.785412
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 4 && endingUnit == 4{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+}
