4991cff28318afcf00cc7eb4e533559d441a140f
Oliver Gane <ogane@rsgc.on.ca>
2016-01-17 20:40:14 -0500

    Changed a whole lot of things. Deleted and restarted all the code and finally got everything working. Not sure what the problem was but I fixed it.

--- Algorithm ISP.swift
+++ Algorithm ISP.swift
@@ -8,97 +8,201 @@
 //
 
 import Foundation
+1
 
 
 print("Welcome to Oliver's Unit Converter!")
 
-var validInput : Bool
+print("\nWhat type of units are you working with today?")
+print("1 - Mass")
+print("2 - Capacitance")
+print("3 - Temperature")
+print("4 - Volume")
+var unitType = Int(readLine(stripNewline: true)!)!
 
-var gram = 1
 
-var ounce = 28.349523
-
-var pound = 453.59237
-
-var kilogram = 1000
-
-repeat {
+if unitType == 1 {
+    print("\nWhat unit are you starting with?")
+    print("1 - Gram")
+    print("2 - Ounce")
+    print("3 - Pound")
+    print("4 - Kilogram")
+    print("Select what type of unit you are starting with (1/2/3/4/Quit)")
     
-    print("\nWhat type of units are you working with today?")
-    print("A - Mass")
-    print("B - Capacitance")
-    print("C - Temperature")
-    print("D - Volume")
-    print("Select what type of unit you want (A/B/C/D/Quit): ", terminator: "")
+    var startingUnit = Int(readLine(stripNewline: true)!)!
     
-    var chosenUnit = readLine(stripNewline: true)
-    validInput = true
-    
-    if let actualUnit = chosenUnit{
+    if startingUnit == 1 {
+        print("What would you like to convert to?")
+        print("1 - Gram")
+        print("2 - Ounce")
+        print("3 - Pound")
+        print("4 - Kilogram")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
         
-        print("")
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
         
-        switch actualUnit {
-            
-        case "A", "a" :
-            print("\nAwesome, what unit are you starting with?")
-            print("A - Gram")
-            print("B - Ounce")
-            print("C - Pound")
-            print("D - Kilogram")
-            print("Select what type of unit you are starting with (A/B/C/D/Quit: ", terminator: "")
-        default:
-            print("Sorry, please re-input letter")
-            validInput = false
+        if startingUnit == 1 && endingUnit == 1{
+            Value = Value*1
+            print("And your new value is......")
+            print(Value)
         }
-        var massStartingUnit = readLine(stripNewline: true)
-        validInput = true
-        
-        
-        if let actualMassStartingUnit = massStartingUnit{
-            print("")
-            
-            switch actualMassStartingUnit {
-            case "A", "a", "B", "b", "C", "c", "D", "d":
-                print("\nOk, what are you converting to?")
-                print("A - Gram")
-                print("B - Ounce")
-                print("C - Pound")
-                print("D - Kilogram")
-                print("Select what type of unit you are converting to (A/B/C/D/Quit: ", terminator: "")
-            default:
-                print("Sorry, please re-input letter.")
-                validInput = false
-            }
-            var massEndingUnit = readLine(stripNewline: true)
-            validInput = true
-            
-            if let actualMassEndingUnit = massEndingUnit{
-                print("")
-                switch actualMassEndingUnit {
-                case "A", "a", "B", "b", "C", "c", "D", "d":
-                    print("\nOk, please enter in the value you want to change.")
-                    
-                    print("Input your value.", terminator: "")
-                default:
-                    print("Sorry, please re-input number.")
-                    validInput = false
-                }
-                
-                
-                
-                
-                //PUT NEW CODE HERE
-                
-                
-                
-            }
+        if startingUnit == 1 && endingUnit == 2{
+            Value = Value * 28.349523
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 1 && endingUnit == 3{
+            Value = Value * 453.59237
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 1 && endingUnit == 4{
+            Value = Value * 1000
+            print("And your new value is......")
+            print(Value)
         }
     }
-} while validInput == false
+    if startingUnit == 2 {
+        print("What would you like to convert to?")
+        print("1 - Gram")
+        print("2 - Ounce")
+        print("3 - Pound")
+        print("4 - Kilogram")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 2 && endingUnit == 1{
+            Value = Value*28.349523
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 2 && endingUnit == 2{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 2 && endingUnit == 3{
+            Value = Value * 0.0625
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 2 && endingUnit == 4{
+            Value = Value * 0.02835
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+    if startingUnit == 3 {
+        print("What would you like to convert to?")
+        print("1 - Gram")
+        print("2 - Ounce")
+        print("3 - Pound")
+        print("4 - Kilogram")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 3 && endingUnit == 1{
+            Value = Value * 453.59237
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 3 && endingUnit == 2{
+            Value = Value * 16
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 3 && endingUnit == 3{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 3 && endingUnit == 4{
+            Value = Value * 0.453592
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+    if startingUnit == 4 {
+        print("What would you like to convert to?")
+        print("1 - Gram")
+        print("2 - Ounce")
+        print("3 - Pound")
+        print("4 - Kilogram")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 4 && endingUnit == 1{
+            Value = Value * 1000
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 4 && endingUnit == 2{
+            Value = Value * 35.273962
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 4 && endingUnit == 3{
+            Value = Value * 2.204623
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 4 && endingUnit == 4{
+            Value = Value * 1
+            print("And your new value is......")
+            print(Value)
+        }
+    }
+}
 
-print("")
-
+if unitType == 2{
+    print("\nWhat unit are you starting with?")
+    print("1 - Gram")
+    print("2 - Ounce")
+    print("3 - Pound")
+    print("4 - Kilogram")
+    print("Select what type of unit you are starting with (1/2/3/4/Quit)")
+    
+    var startingUnit = Int(readLine(stripNewline: true)!)!
+    
+    if startingUnit == 1 {
+        print("What would you like to convert to?")
+        print("1 - Gram")
+        print("2 - Ounce")
+        print("3 - Pound")
+        print("4 - Kilogram")
+        var endingUnit = Int(readLine(stripNewline: true)!)!
+        
+        print("\nOk, please enter in the value you want to change.")
+        var Value = Double(readLine(stripNewline: true)!)!
+        
+        if startingUnit == 1 && endingUnit == 1{
+            Value = Value*1
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 1 && endingUnit == 2{
+            Value = Value * 28.349523
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 1 && endingUnit == 3{
+            Value = Value * 453.59237
+            print("And your new value is......")
+            print(Value)
+        }
+        if startingUnit == 1 && endingUnit == 4{
+            Value = Value * 1000
+            print("And your new value is......")
+            print(Value)
+        }
+}
 
 
 
